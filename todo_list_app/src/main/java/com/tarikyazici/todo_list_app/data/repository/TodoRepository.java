5e6993aa0bf166980e77b70a5a7c8a47b525efab
Muhammed Tarık Yazıcı <77811323+trkyzc@users.noreply.github.com>
2025-01-11 02:15:45 +0300

    feat(repository): Add repository classes

--- todo_list_app/src/main/java/com/tarikyazici/todo_list_app/data/repository/TodoRepository.java
+++ todo_list_app/src/main/java/com/tarikyazici/todo_list_app/data/repository/TodoRepository.java
@@ -1,9 +1,16 @@
 package com.tarikyazici.todo_list_app.data.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tarikyazici.todo_list_app.data.entity.Todo;
 
+@Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+	
+	 List<Todo> findByUserId(Long userId);
+	 List<Todo> findByIsCompleted(boolean isCompleted);
 
 }
--- todo_list_app/src/main/java/com/tarikyazici/todo_list_app/data/repository/UserRepository.java
+++ todo_list_app/src/main/java/com/tarikyazici/todo_list_app/data/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.tarikyazici.todo_list_app.data.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tarikyazici.todo_list_app.data.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	
+	 boolean existsByUsername(String username);
+	 boolean existsByEmail(String email);
 
 }
